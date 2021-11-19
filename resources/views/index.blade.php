98cb7310f6a8e8cfb50e2927fc9219ba4e5efb98
neha341 <nehawagh5@gmail.com>
2021-11-19 19:36:15 +0530

    changes in title

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="csrf-token" content="{{csrf_token() }}">
 
-	<title>Okela</title>
+	<title>NVS _ DEMO</title>
 
 	<link rel="stylesheet" type="text/css" href="{{url('public/assets/css/bootstrap.min.css')}}">
 	<link rel="stylesheet" type="text/css" href="{{url('public/assets/css/style.css')}}">
